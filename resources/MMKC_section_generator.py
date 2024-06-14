e1ddb17ec47dadccbbbc0d4708a2add124fb374b
Triach Rold <156170660+triach-rold@users.noreply.github.com>
2024-06-14 13:04:59 +0530

    Compressed images

--- resources/MMKC_section_generator.py
+++ resources/MMKC_section_generator.py
@@ -1,15 +1,16 @@
 template = '''<section class="img-section">
     <div class="{wrap_class}">
-        <div class="img" style="background-image: url('../resources/meetup/MMKCMeetup-{num:02d}.jpg');"></div>
+        <img class="img lazy" data-src="resources/MMKCMeetup-{num:02d}.jpg" loading="lazy" alt="MMKC Meetup {num:02d}">
     </div>
     <div class="{proj_info_class}">
         <span class="wrap"><h1 class="info-text">{num:02d}</h1></span>
-        <span class="wrap"><h1 class="info-text">Furniture</h1></span>
+        <span class="wrap"><h1 class="info-text">Keyboard</h1></span>
         <span class="wrap"><h1 class="info-text">Mumbai</h1></span>
         <span class="wrap"><h1 class="info-text">2024</h1></span>
         <span class="wrap"><h1 class="info-text">MMKC2024</h1></span>
     </div>
 </section>'''
+
 pattern = [1, 2]
 sections = []
 for i in range(1, 68):
@@ -20,7 +21,6 @@ for i in range(1, 68):
     sections.append(section)
 
 html_content = '\n'.join(sections)
-print(html_content)
 
 with open('generated_sections.html', 'w') as file:
     file.write(html_content)
